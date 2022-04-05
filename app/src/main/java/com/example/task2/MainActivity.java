ee0417f2b13f54d14f6106444d15739a7c3d63df
Suraj Sah <surajsah6041@gmail.com>
2022-04-05 08:40:06 +0545

    homework completed

--- app/src/main/java/com/example/task2/MainActivity.java
+++ app/src/main/java/com/example/task2/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private int mCount;
+    private Button btn_1,btn_2;
 
     {
         mCount = 0;
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        btn_1= findViewById(R.id.button3);
+        btn_2=findViewById(R.id.button2);
     }
     public void showToast(View view){
         Toast.makeText(this, "Hello Toast", Toast.LENGTH_SHORT).show();
@@ -30,6 +34,17 @@ public class MainActivity extends AppCompatActivity {
         mCount++;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+         btn_1.setBackgroundResource(R.color.pink);
+         btn_2.setBackgroundResource(R.color.green);
 
     }
-}
+
+    public void Zero(View view) {
+        mCount=0;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+         btn_1.setBackgroundResource(R.color.grey);
+
+    }
+
+    }
